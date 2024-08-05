58534eaacbda8644b9bb6c1dc9a1ee3aa05c8899
Harits <mharitsnf@gmail.com>
2024-08-05 18:59:38 +0200

    Implement no collision snapping

---
+++ addons/extra_snaps/common.gd
@@ -0,0 +1,27 @@
+@tool
+class_name ESCommon extends RefCounted
+
+enum RootItems {
+    SURFACE_TYPE_SUBMENU,
+    SNAP_TYPE_SUBMENU,
+    CONFIGURE_MASK
+}
+
+enum SurfaceTypes {
+    COLLISION_OBJECTS,
+    MESHES,
+}
+
+enum SnapTypes {
+    SNAP_TO_SURFACE,
+    SNAP_ALONG_NORMAL
+}
+
+const DIALOG_CONFIGURE_MASK_SCENE: PackedScene = preload("./dialog_configure_mask.tscn")
+const ES_MENU_BUTTON_PSCN: PackedScene = preload("./es_menu_button.tscn")
+const SETTINGS_FILE_PATH = "user://extra_snaps.cfg"
+
+# https://github.com/godotengine/godot-proposals/issues/2411
+const INT32_MAX = 4294967295
+# https://docs.godotengine.org/en/stable/classes/class_float.html#
+const FLOAT64_MAX = 1.79769e308
