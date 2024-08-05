e51ffa1074641428157edc141913937bfcfa19a4
Harits <mharitsnf@gmail.com>
2024-08-05 18:59:38 +0200

    Implement no snap collision

--- addons/extra_snaps/es_menu_button.gd
+++ addons/extra_snaps/es_menu_button.gd
@@ -1,1 +1,76 @@
+@tool
 class_name ESMenuButton extends MenuButton
+
+const common = preload('./common.gd')
+
+var surface_type_pm: PopupMenu
+var snap_type_pm: PopupMenu
+var pm: PopupMenu
+var dialog_configure_mask: Window
+
+var collision_mask: int = common.INT32_MAX
+
+signal configure_mask_window_instantiated(window: Window)
+signal new_snap_type_selected(id: common.SnapTypes)
+signal new_surface_type_selected(id: common.SurfaceTypes)
+signal collision_mask_changed(mask: int)
+
+func _enter_tree() -> void:
+	# Setup surface type submenu
+	surface_type_pm = PopupMenu.new()
+	surface_type_pm.name = "SurfaceTypePM"
+	surface_type_pm.add_radio_check_item("Snap with Collision Objects", common.SurfaceTypes.COLLISION_OBJECTS)
+	surface_type_pm.add_radio_check_item("Snap with Meshes", common.SurfaceTypes.MESHES)
+	surface_type_pm.set_item_checked(common.SurfaceTypes.COLLISION_OBJECTS, true)
+	surface_type_pm.id_pressed.connect(_on_surface_type_pm_id_pressed)
+
+	# Setup snap type submenu
+	snap_type_pm = PopupMenu.new()
+	snap_type_pm.name = "SnapTypePM"
+	snap_type_pm.add_radio_check_item("Snap to Surface", common.SnapTypes.SNAP_TO_SURFACE)
+	snap_type_pm.add_radio_check_item("Snap Along Normals", common.SnapTypes.SNAP_ALONG_NORMAL)
+	snap_type_pm.set_item_checked(common.SnapTypes.SNAP_TO_SURFACE, true)
+	snap_type_pm.id_pressed.connect(_on_snap_type_pm_id_pressed)
+
+	# Setup main popup
+	pm = get_popup()
+	pm.add_child(surface_type_pm)
+	pm.add_child(snap_type_pm)
+	pm.add_submenu_item("Surface Type", "SurfaceTypePM", common.RootItems.SURFACE_TYPE_SUBMENU)
+	pm.add_submenu_item("Snap Type", "SnapTypePM", common.RootItems.SNAP_TYPE_SUBMENU)
+	pm.add_item("Configure Collision Mask", common.RootItems.CONFIGURE_MASK)
+	pm.id_pressed.connect(_on_main_pm_id_pressed)
+
+func _exit_tree() -> void:
+	if dialog_configure_mask:
+		dialog_configure_mask.queue_free()
+
+func _on_surface_type_pm_id_pressed(id: int) -> void:
+	new_surface_type_selected.emit(id)
+	for i: int in common.SurfaceTypes.values():
+		surface_type_pm.set_item_checked(i, i == id)
+
+func _on_snap_type_pm_id_pressed(id: int) -> void:
+	new_snap_type_selected.emit(id)
+	for i: int in common.SnapTypes.values():
+		snap_type_pm.set_item_checked(i, i == id)
+
+func _on_main_pm_id_pressed(id: int) -> void:
+	match id:
+		common.RootItems.CONFIGURE_MASK:
+			dialog_configure_mask = common.DIALOG_CONFIGURE_MASK_SCENE.instantiate()
+			dialog_configure_mask.connect("on_confirm", _on_mask_config_confirm)
+			dialog_configure_mask.set("mask", collision_mask)
+			dialog_configure_mask.close_requested.connect(_on_close_config)
+			dialog_configure_mask.hide()
+			configure_mask_window_instantiated.emit(dialog_configure_mask)
+		
+func _on_mask_config_confirm(mask: int) -> void:
+	collision_mask_changed.emit(mask)
+	dialog_configure_mask.queue_free()
+	if mask == collision_mask:
+		return
+	collision_mask = mask
+
+func _on_close_config() -> void:
+	dialog_configure_mask.queue_free()
--- addons/extra_snaps/extra_snaps.gd
+++ addons/extra_snaps/extra_snaps.gd
@@ -1,39 +1,32 @@
 @tool
 extends EditorPlugin
 
+const common = preload('./common.gd')
+
+var tool_button: ESMenuButton
+
 var new_config = ConfigFile.new()
-const SETTINGS_FILE_PATH = "user://extra_snaps.cfg"
 var has_config = false
 
 var undoredo_action: EditorUndoRedoManager = get_undo_redo()
 
-var tool_button: ESMenuButton
-var pm: PopupMenu
+var current_surface_type: common.SurfaceTypes = common.SurfaceTypes.COLLISION_OBJECTS
+var current_snap_type: common.SnapTypes = common.SnapTypes.SNAP_TO_SURFACE
 
-enum SnapType { SNAP_TO_SURFACE, SNAP_ALONG_NORMAL }
-var current_snap_type: SnapType = SnapType.SNAP_TO_SURFACE
-var snap_type_labels: Dictionary = {
-	SnapType.SNAP_TO_SURFACE: "Snap to Surface",
-	SnapType.SNAP_ALONG_NORMAL: "Snap Along Normal",
-}
 var selected: Node3D = null
 var has_moved: bool = false
 var move_pressed: bool = false
 
-# https://github.com/godotengine/godot-proposals/issues/2411
-const INT32_MAX = 4294967295
-# All collisions are enabled by default
-var collision_mask: int = INT32_MAX
-var DialogConfigureMaskScene = preload("res://addons/extra_snaps/dialog_configure_mask.tscn")
-var dialog_configure_mask: Window
-const ConfigureDialogToolButtonId = 2
+var collision_mask: int = common.INT32_MAX
+
+# region Lifecycle functions
 
 func _enter_tree() -> void:
 	# Initialize Configuration File
-	var err = new_config.load(SETTINGS_FILE_PATH)
+	var err = new_config.load(common.SETTINGS_FILE_PATH)
 	if err != OK:
 		if err == ERR_FILE_NOT_FOUND:
-			err = new_config.save(SETTINGS_FILE_PATH)
+			err = new_config.save(common.SETTINGS_FILE_PATH)
 		if err != OK:
 			print("ExtraSnaps: loading config file failed: " + str(err))
 	if err == OK:
@@ -42,17 +35,13 @@ func _enter_tree() -> void:
 		collision_mask = new_config.get_value("collision_mask", "collision_mask", collision_mask)
 
 	# Setup tool button
-	tool_button = preload("res://addons/extra_snaps/es_menu_button.tscn").instantiate()
+	tool_button = common.ES_MENU_BUTTON_PSCN.instantiate()
+	tool_button.collision_mask = collision_mask
+	tool_button.new_surface_type_selected.connect(_on_new_surface_type_selected)
+	tool_button.new_snap_type_selected.connect(_on_new_snap_type_selected)
+	tool_button.configure_mask_window_instantiated.connect(_on_configure_mask_window_instantiated)
+	tool_button.collision_mask_changed.connect(_on_collision_mask_changed)
 	add_control_to_container(CONTAINER_SPATIAL_EDITOR_MENU, tool_button)
-	
-	# Setup popup menu
-	pm = tool_button.get_popup()
-	pm.add_radio_check_item(snap_type_labels[SnapType.SNAP_TO_SURFACE], SnapType.SNAP_TO_SURFACE)
-	pm.add_radio_check_item(snap_type_labels[SnapType.SNAP_ALONG_NORMAL], SnapType.SNAP_ALONG_NORMAL)
-	pm.add_separator()
-	pm.add_item("Configure Mask", ConfigureDialogToolButtonId)
-	pm.set_item_checked(SnapType.SNAP_TO_SURFACE, true)
-	pm.id_pressed.connect(_on_popup_id_pressed)
 
 	# Configure shortcut key
 	InputMap.add_action("extra_snaps_move")
@@ -65,54 +54,64 @@ func _enter_tree() -> void:
 func _exit_tree() -> void:
 	remove_control_from_container(CONTAINER_SPATIAL_EDITOR_MENU, tool_button)
 	tool_button.queue_free()
-	dialog_configure_mask.queue_free()
 
 	InputMap.erase_action("extra_snaps_move")
 
 var selected_children: Array[Node] = []
+var visual_instances_data: Array[Dictionary] = []
 func _handles(object: Object) -> bool:
 	if object is Node3D:
 		selected = object
+		visual_instances_data = []
+		collect_global_tris(object)
+
 		var out: Array[Node] = []
-		get_all_children(out, object, [CollisionObject3D, CSGShape3D])
+		get_all_children(out, object, null, [CollisionObject3D, CSGShape3D])
 		selected_children = out
 		return true
 
 	selected = null
 	selected_children = []
+	visual_instances_data = []
 	return false
 
 var csg_use_collisions: Array[Dictionary] = []
 func _forward_3d_gui_input(viewport_camera: Camera3D, event: InputEvent) -> int:
 	move_pressed = Input.is_action_pressed("extra_snaps_move")
 
+	# On complete movement
 	if Input.is_action_just_released("extra_snaps_move") and has_moved:
 		for csg_data: Dictionary in csg_use_collisions:
 			(csg_data['node'] as CSGShape3D).use_collision = csg_data['use_collision']
 		
 		csg_use_collisions = []
 
-		undoredo_action.add_do_property(selected, "global_position", Vector3(selected.global_position))
-		undoredo_action.add_do_property(selected, "global_basis", Basis(selected.global_basis))
+		undoredo_action.add_do_property(selected, "global_transform", Transform3D(selected.global_transform))
 		undoredo_action.commit_action()
 		has_moved = false
 
+	# During movement
 	if move_pressed and selected:
 		if event is InputEventMouseMotion:
 			return _move_selection(viewport_camera, event)
 
 	return AFTER_GUI_INPUT_PASS
 
-func _close_config() -> void:
-	dialog_configure_mask.queue_free()
+# region ES Menu Button Signal Listener
 
-func save_config():
-	if has_config:
-		new_config.set_value("collision_mask", "collision_mask", collision_mask)
-		new_config.save(SETTINGS_FILE_PATH)
+func _on_configure_mask_window_instantiated(window: Window) -> void:
+	var editor_interface = get_editor_interface()
+	var base_control = editor_interface.get_base_control()
+	base_control.add_child(window)
+	window.popup_centered()
 
-func on_mask_config_confirm(mask: int) -> void:
-	dialog_configure_mask.queue_free()
+func _on_new_surface_type_selected(id: common.SurfaceTypes) -> void:
+	current_surface_type = id
+
+func _on_new_snap_type_selected(id: common.SnapTypes) -> void:
+	current_snap_type = id
+
+func _on_collision_mask_changed(mask: int) -> void:
 	if mask == collision_mask:
 		return
 	var old_mask = collision_mask
@@ -125,22 +124,12 @@ func on_mask_config_confirm(mask: int) -> void:
 	undoredo_action.commit_action()
 	save_config()
 
-func _on_popup_id_pressed(id: int) -> void:
-	if id == ConfigureDialogToolButtonId:
-		dialog_configure_mask = DialogConfigureMaskScene.instantiate()
-		dialog_configure_mask.connect("on_confirm", on_mask_config_confirm)
-		dialog_configure_mask.set("mask", collision_mask)
-		dialog_configure_mask.close_requested.connect(_close_config)
-		dialog_configure_mask.hide()
-		var editor_interface = get_editor_interface()
-		var base_control = editor_interface.get_base_control()
-		base_control.add_child(dialog_configure_mask)
-		dialog_configure_mask.popup_centered()
+func save_config():
+	if has_config:
+		new_config.set_value("collision_mask", "collision_mask", collision_mask)
+		new_config.save(common.SETTINGS_FILE_PATH)
 
-	if SnapType.values().has(id):
-		current_snap_type = id
-		for i in SnapType.values():
-			pm.set_item_checked(i, i == id)
+# region Movement handling
 
 const RAY_LENGTH: float = 1000.
 func _move_selection(viewport_camera: Camera3D, event: InputEventMouseMotion) -> int:
@@ -165,10 +154,61 @@ func _move_selection(viewport_camera: Camera3D, event: InputEventMouseMotion) ->
 				})
 				child.use_collision = false
 		
-		undoredo_action.add_undo_property(selected, "global_position", Vector3(selected.global_position))
-		undoredo_action.add_undo_property(selected, "global_basis", Basis(selected.global_basis))
+		undoredo_action.add_undo_property(selected, "global_transform", Transform3D(selected.global_transform))
 		has_moved = true
-	
+
+	match current_surface_type:
+		common.SurfaceTypes.COLLISION_OBJECTS: _collision_objects_snapping(viewport_camera, event)
+		common.SurfaceTypes.MESHES: _mesh_snapping(viewport_camera, event)
+
+	return AFTER_GUI_INPUT_STOP
+
+func _mesh_snapping(viewport_camera: Camera3D, event: InputEventMouseMotion) -> void:
+	# Mesh snapping
+	var from: Vector3 = viewport_camera.project_ray_origin(event.position)
+	var to: Vector3 = viewport_camera.project_ray_normal(event.position)
+
+	var min_t: float = common.FLOAT64_MAX
+	var min_p: Vector3 = Vector3.INF
+	var min_n: Vector3
+
+	var data_to_process: Array[Dictionary] = []
+
+	# Check if aabb of visual instance intersects
+	for data: Dictionary in visual_instances_data:
+		var global_aabb: AABB = data['aabb']
+		var res: Variant = global_aabb.intersects_ray(from, to)
+		if res is Vector3:
+			data_to_process.append(data)
+
+	for data: Dictionary in data_to_process:
+		var tris: PackedVector3Array = data['tris']
+		for i: int in range(0, tris.size(), 3):
+			var v0: Vector3 = tris[i + 0]
+			var v1: Vector3 = tris[i + 1]
+			var v2: Vector3 = tris[i + 2]
+			var res: Variant = Geometry3D.ray_intersects_triangle(from, to, v2, v1, v0)
+			if res is Vector3:
+				var len: float = from.distance_to(res)
+				if len < min_t:
+					min_t = len
+					min_p = res
+					var v0v1: Vector3 = v1 - v0
+					var v0v2: Vector3 = v2 - v0
+					min_n = v0v2.cross(v0v1)
+
+	if min_t >= common.FLOAT64_MAX: return
+
+	match current_snap_type:
+		common.SnapTypes.SNAP_TO_SURFACE:
+			selected.global_position = min_p
+		common.SnapTypes.SNAP_ALONG_NORMAL:
+			selected.global_position = min_p
+			var g_quat: Quaternion = get_quaternion_from_normal(selected.global_basis, min_n)
+			selected.global_basis = Basis(g_quat)
+
+func _collision_objects_snapping(viewport_camera: Camera3D, event: InputEventMouseMotion) -> void:
+	# Physics snapping
 	var from: Vector3 = viewport_camera.project_ray_origin(event.position)
 	var to: Vector3 = from + viewport_camera.project_ray_normal(event.position) * RAY_LENGTH
 	var space: PhysicsDirectSpaceState3D = viewport_camera.get_world_3d().direct_space_state
@@ -195,22 +235,60 @@ func _move_selection(viewport_camera: Camera3D, event: InputEventMouseMotion) ->
 	var result: Dictionary = space.intersect_ray(ray_query)
 	
 	match current_snap_type:
-		SnapType.SNAP_TO_SURFACE:
+		common.SnapTypes.SNAP_TO_SURFACE:
 			if result.has("position"):
 				selected.global_position = result.position
 			
-		SnapType.SNAP_ALONG_NORMAL:
+		common.SnapTypes.SNAP_ALONG_NORMAL:
 			if result.has("position"):
 				selected.global_position = result.position
 			if result.has("normal"):
 				var g_quat: Quaternion = get_quaternion_from_normal(selected.global_basis, result.normal)
 				selected.global_basis = Basis(g_quat)
 
-	return AFTER_GUI_INPUT_STOP
+# region Common functions
+
+## Get global triangle of all nodes in the scene, except the [exclude] node and its children.
+func collect_global_tris(exclude: Node) -> void:
+	var nodes: Array[Node] = []
+	get_all_children(nodes, EditorInterface.get_edited_scene_root(), exclude, [MeshInstance3D, CSGShape3D])
+	for node: Node in nodes:
+		if node is MeshInstance3D:
+			var mesh: Mesh = node.mesh
+			if !mesh: continue
+			
+			var aabb: AABB = node.global_transform * node.get_aabb()
+			var tris: PackedVector3Array = []
+			
+			var verts: PackedVector3Array = mesh.get_faces()
+			for vert: Vector3 in verts:
+				tris.append(node.global_transform * vert)
+
+			visual_instances_data.append({ "node": node, "aabb": aabb, "tris": tris })
+
+		elif node is CSGShape3D:
+			var meshes: Array = node.get_meshes()
+			if meshes.is_empty(): continue
+
+			var aabb: AABB = node.global_transform * node.get_aabb()
+			var tris: PackedVector3Array = []
+
+			var verts: PackedVector3Array = (meshes[1] as ArrayMesh).get_faces()
+			for vert: Vector3 in verts:
+				tris.append(node.global_transform * vert)
+
+			visual_instances_data.append({ "node": node, "aabb": aabb, "tris": tris })
+
+## Transform local triangle to global triangle.
+func _local_tri_to_global_tri(trf: Transform3D, tri: Vector3) -> Vector3:
+	return trf * tri
 
 ## Returns all the children of [node] recursively. Limit to specific types using [types]. 
-func get_all_children(out: Array[Node], node: Node, types: Array[Variant] = []) -> void:
+func get_all_children(out: Array[Node], node: Node, exclude: Node = null, types: Array[Variant] = []) -> void:
+	if node == exclude: return
 	for child: Node in node.get_children():
+		if child == exclude: continue
+
 		if types.is_empty():
 			out.append(child)
 		else:
@@ -220,8 +298,7 @@ func get_all_children(out: Array[Node], node: Node, types: Array[Variant] = []) 
 					break
 		
 		if child.get_child_count() > 0:
-			get_all_children(out, child, types)
-
+			get_all_children(out, child, exclude, types)
 
 func get_quaternion_from_normal(old_basis: Basis, new_normal: Vector3) -> Quaternion:
 	new_normal = new_normal.normalized()
